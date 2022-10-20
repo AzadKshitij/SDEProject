16536b6bb9fdb349db8b06a20b1b53302459819d
Sandeep Swami <swami.2@iitj.ac.in>
2022-10-21 00:03:24 +0530

    Added Editor

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import Blogs from "./components/Blogs";
-import Editorbox from "./components/Editor/Editorbox";
+import Editor from "./Editor";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
 				<Header />
 				<Routes>
 					<Route path="/" element={<Blogs />}></Route>
-					<Route exact path="/newblog" element={<Editorbox />}></Route>
+					<Route exact path="/newblog" element={<Editor />}></Route>
 				</Routes>
 			</Router>
 
--- Frontend/src/components/Blogs.jsx
+++ Frontend/src/components/Blogs.jsx
@@ -4,63 +4,54 @@ import BlogCard from "./BlogCard";
 import service from "../service";
 
 function Blogs() {
-  const [blogs, setBlogs] = React.useState([]);
+	const [blogs, setBlogs] = React.useState([]);
 
-  React.useEffect(() => {
-    getAllData();
-  }, []);
+	React.useEffect(() => {
+		getAllData();
+	}, []);
 
-  async function getAllData() {
-    try {
-      const res = await service.get("/post/get");
+	async function getAllData() {
+		try {
+			const res = await service.get("/post/get");
 
-      const result = {
-        status: res.status + "-" + res.statusText,
-        headers: res.headers,
-        data: res.data,
-      };
+			const result = {
+				status: res.status + "-" + res.statusText,
+				headers: res.headers,
+				data: res.data,
+			};
+			setBlogs(result.data);
+			console.log("==================================");
+			console.log(JSON.stringify(result, null, 2));
+			console.log("==================================");
+		} catch (err) {
+			console.log(JSON.stringify(err, null, 2));
+		}
+	}
 
-      setBlogs(result.data);
-
-      console.log("==================================");
-      console.log(JSON.stringify(result, null, 2));
-      console.log("==================================");
-    } catch (err) {
-      console.log(JSON.stringify(err, null, 2));
-    }
-  }
-
-  return (
-    <div className="container mx-auto bg-bg px-10">
-      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12  ">
-        <div className=" col-span-1 lg:col-span-8 ">
-          {blogs.map((blog) => (
-            <BlogCard
-              key={blog._id["$oid"]}
-              title={blog.title}
-              summary={blog.summary}
-              image={blog.mainImage}
-              publishedAt={blog.publishedAt["$date"]}
-              author={blog.author}
-              id={blog._id}
-            />
-          ))}
-
-          {/* <BlogCard />
-          <BlogCard />
-          <BlogCard />
-          <BlogCard />
-          <BlogCard />
-          <BlogCard /> */}
-        </div>
-        <div className=" col-span-1  lg:col-span-4 ">
-          <div className=" relative top-8 lg:sticky ">
-            <h1>Sandeep Swami</h1>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="container mx-auto bg-bg px-10">
+			<div className="grid grid-cols-1 gap-12 lg:grid-cols-12  ">
+				<div className=" col-span-1 lg:col-span-8 ">
+					{blogs.map((blog) => (
+						<BlogCard
+							key={blog._id["$oid"]}
+							title={blog.title}
+							summary={blog.summary}
+							image={blog.mainImage}
+							publishedAt={blog.publishedAt["$date"]}
+							author={blog.author}
+							id={blog._id}
+						/>
+					))}
+				</div>
+				<div className=" col-span-1  lg:col-span-4 ">
+					<div className=" relative top-8 lg:sticky ">
+						<h1>Sandeep Swami</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Blogs;
